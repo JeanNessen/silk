088ad90af51c7511b960e057d00103b947214fdd
JeanNessen <jean.luc.nessen@gmail.com>
2026-07-22 20:59:58 +0200

    Move parse_file into the app, so parser does not depend on io anymore.

---
+++ app/handler.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <print>
+
+#include <slk/core/types.h>
+
+class StdOutHandler final
+{
+public:
+    void OnAdd(silk::OrderAdd) { ++m_numAdd; }
+    void OnAddMPID(silk::OrderAddMPID) { ++m_numAddMPID; }
+    void OnExecuted(silk::OrderExecuted) { ++m_numExecuted; }
+    void OnExecutedWithPrice(silk::OrderExecutedWithPrice)
+    {
+        ++m_numExecutedWithPrice;
+    }
+    void OnCanceled(silk::OrderCanceled) { ++m_numCanceled; }
+    void OnDeleted(silk::OrderDeleted) { ++m_numDeleted; }
+    void OnReplaced(silk::OrderReplaced) { ++m_numReplaced; }
+    void OnSkipped() { ++m_numSkipped; }
+
+    void PrintResults() const
+    {
+        std::println("Num Adds:              {}", m_numAdd);
+        std::println("Num AddsMPID:          {}", m_numAddMPID);
+        std::println("Num Executed:          {}", m_numExecuted);
+        std::println("Num ExecutedWithPrice: {}", m_numExecutedWithPrice);
+        std::println("Num Canceled:          {}", m_numCanceled);
+        std::println("Num Deleted:           {}", m_numDeleted);
+        std::println("Num Replaced:          {}", m_numReplaced);
+        std::println("Num Skipped:           {}", m_numSkipped);
+    }
+
+private:
+private:
+    size_t m_numAdd               = 0;
+    size_t m_numAddMPID           = 0;
+    size_t m_numExecuted          = 0;
+    size_t m_numExecutedWithPrice = 0;
+    size_t m_numCanceled          = 0;
+    size_t m_numDeleted           = 0;
+    size_t m_numReplaced          = 0;
+    size_t m_numSkipped           = 0;
+};
